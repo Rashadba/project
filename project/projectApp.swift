c6e7d26842855705e58aafff544647c406dce7bb
Rashadba <hairamark@mail.ru>
2022-10-06 16:23:57 +0300

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by user on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
